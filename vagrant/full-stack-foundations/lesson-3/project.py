1a43f43f15a0370afa3cd9fc12524df1ed816fed
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2016-01-08 10:20:02 -0200

    lesson 3: template preparado

--- vagrant/full-stack-foundations/lesson-3/project.py
+++ vagrant/full-stack-foundations/lesson-3/project.py
@@ -16,10 +16,16 @@ session = DBSession()
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    restaurant = session.query(Restaurant).first()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    output = ''
+    return 'Hello World!'
+
+@app.route('/restaurant/<int:restaurant_id>')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = '<h1>Restaurant: ' + restaurant.name + '</h1>'
     for i in items:
+        output += str(i.id)
+        output += '<br />'
         output += i.name
         output += '<br />'
         output += i.description
@@ -28,6 +34,21 @@ def HelloWorld():
         output += '<br />'
         output += '<br />'
     return output
+
+# Task 1: Create route for newMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+   return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+   return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+   return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
