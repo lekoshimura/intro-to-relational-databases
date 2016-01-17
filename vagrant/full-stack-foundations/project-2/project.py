15006f50f3609aa873e8ccae78077ec87a0b1bd4
Leonardo Koshimura <leonardo.koshimura@gmail.com>
2016-01-17 05:24:42 +0000

    project 2: list, views e creates completos

--- vagrant/full-stack-foundations/project-2/project.py
+++ vagrant/full-stack-foundations/project-2/project.py
@@ -46,23 +46,71 @@ def puppyCreate():
         return redirect(url_for('puppyView', puppy_id = puppy.id))
     else:
         return render_template('puppy-create.html');
-    
-
+        
+        
+        
 @app.route('/shelter/list')
 def shelterList():
     shelters = session.query(Shelter).all()
     return render_template('shelter-list.html', shelters = shelters)
+    
+@app.route('/shelter/view/<int:shelter_id>')
+def shelterView(shelter_id):
+    shelter = session.query(Shelter).get(shelter_id)
+    return render_template('shelter-view.html', shelter = shelter)
+
+@app.route('/shelter/create', methods = ['GET', 'POST'])    
+def shelterCreate():
+    if request.method == 'POST':
+        shelter = Shelter(
+            name = request.form['name'],
+            address = request.form['address'],
+            city = request.form['city'],
+            state = request.form['state'],
+            zipCode = request.form['zipCode'],
+            website = request.form['website'],
+            email = request.form['email'],
+            maximumCapacity = request.form['maximumCapacity']
+        );
+        session.add(shelter);
+        session.commit();
+        flash('New shelter created');
+        return redirect(url_for('shelterView', shelter_id = shelter.id))
+    else:
+        return render_template('shelter-create.html');
+    
+
 
 @app.route('/adopter/list')
 def adopterList():
     adopters = session.query(Adopter).all()
     return render_template('adopter-list.html', adopters = adopters)
 
+@app.route('/adopter/view/<int:adopter_id>')
+def adopterView(adopter_id):
+    adopter = session.query(Adopter).get(adopter_id);
+    return render_template('adopter-view.html', adopter = adopter)
+
+@app.route('/adopter/create', methods = ['GET', 'POST'])
+def adopterCreate():
+    if request.method == 'POST':
+        adopter = Adopter(
+            name = request.form['name']
+        );
+        session.add(adopter);
+        session.commit();
+        flash('New adopter created');
+        return redirect(url_for('adopterView', adopter_id = adopter.id))
+    else:
+        return render_template('adopter-create.html')
+
 @app.route('/adopter/<int:adopter_id>/puppies')
 def puppiesByAdopter(adopter_id):
     puppies = session.query(PuppyAdopter).filter_by(adopter_id = adopter_id).all()
     adopter = session.query(Adopter).get(adopter_id)
     return render_template('puppies-by-adopter.html', adopter = adopter, puppies = puppies)
+
+
 
 # http://stackoverflow.com/questions/26080872/secret-key-not-set-in-flask-session
 # Note to Heroku [|c0.io] users ending up here: I did not get the example here 
@@ -71,7 +119,6 @@ def puppiesByAdopter(adopter_id):
 # if __name__ == "__main__": block is never entered.
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
-
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 8080)
