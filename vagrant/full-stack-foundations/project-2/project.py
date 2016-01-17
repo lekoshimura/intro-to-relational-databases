72498b4a3380a8b781e904d3d62a170764368ed7
Leonardo Koshimura <leonardo.koshimura@gmail.com>
2016-01-17 02:38:18 +0000

    project 2: puppy create

--- vagrant/full-stack-foundations/project-2/project.py
+++ vagrant/full-stack-foundations/project-2/project.py
@@ -8,6 +8,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from puppy import Base, Puppy, Shelter, PuppyProfile, Adopter, PuppyAdopter
 
+from datetime import datetime
+
 engine = create_engine('sqlite:///puppyshelter.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -21,6 +23,30 @@ def index():
 def puppyList():
     puppies = session.query(Puppy).all()
     return render_template('puppy-list.html', puppies = puppies)
+    
+@app.route('/puppy/view/<int:puppy_id>')
+def puppyView(puppy_id):
+    puppy = session.query(Puppy).get(puppy_id)
+    return render_template('puppy-view.html', puppy = puppy)
+
+@app.route('/puppy/create', methods = ['GET', 'POST'])
+def puppyCreate():
+    if request.method == 'POST':
+        sDateOfBirth = datetime.strptime(request.form['dateOfBirth'], '%d/%m/%Y')
+        puppy = Puppy(
+            name = request.form['name'], 
+            dateOfBirth = sDateOfBirth,
+            breed = request.form['breed'],
+            gender = request.form['gender'],
+            weight = request.form['weight']
+        )
+        session.add(puppy)
+        session.commit()
+        flash('New puppy created')
+        return redirect(url_for('puppyView', puppy_id = puppy.id))
+    else:
+        return render_template('puppy-create.html');
+    
 
 @app.route('/shelter/list')
 def shelterList():
@@ -38,6 +64,14 @@ def puppiesByAdopter(adopter_id):
     adopter = session.query(Adopter).get(adopter_id)
     return render_template('puppies-by-adopter.html', adopter = adopter, puppies = puppies)
 
+# http://stackoverflow.com/questions/26080872/secret-key-not-set-in-flask-session
+# Note to Heroku [|c0.io] users ending up here: I did not get the example here 
+# to work until I moved app.secret_key = ... out of the if block – which in 
+# hindsight makes sense since Heroku runs the app via gunicorn, which means the 
+# if __name__ == "__main__": block is never entered.
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 8080)
